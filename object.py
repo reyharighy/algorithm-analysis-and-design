f10c4ff5ddcdd98dbf0b1d23d489c771e474f857
reyharighy <arighymoch@gmail.com>
2025-06-02 17:05:49 +0700

    big changes
    Details:
    - add color attribute to update
    - exclude edge attribute to be updated through update_attributes because it has its own functionality when updated
    - add bunch of functions when obtaining the value of each attribute
    - set all attibutes member to be private so it can't be mutated outside the class

--- object.py
+++ object.py
@@ -8,48 +8,74 @@ class VertexAttributes(TypedDict, total=False):
     predecessor: 'Vertex | None'
     distance: int | float
     visited: bool
+    color: str
 
 class Vertex:
     """A class representing a vertex in a graph."""
 
     def __init__(self, label: str):
-        self.label: str = label
-        self.predecessor: Vertex | None = None
-        self.edges: list[Edge] = []
-        self.distance: int | float = float('inf')
-        self.visited: bool = False
+        self.__label: str = label
+        self.__edges: list[Edge] = []
+
+        # Default attributes
+        self.__predecessor: Vertex | None = None
+        self.__distance: int | float = float('inf')
+        self.__visited: bool = False
+        self.__color: str = 'white'
 
     def __repr__(self) -> str:
         """Returns a string representation of the vertex."""
         s = "Vertex({"
-        s += f"label: {self.label}"
+        s += f"label: {self.__label}"
         s += ", predecessor: "
 
-        if self.predecessor:
-            s += f"Vertex({self.predecessor.label})"
+        if self.__predecessor:
+            s += f"Vertex({self.__predecessor.get_label()})"
         else:
             s += "None"
 
-        s += f", edges: {self.edges}"
-        s += f", distance: {self.distance}"
-        s += f", visited: {self.visited}"
+        s += f", edges: {self.__edges}"
+        s += f", distance: {self.__distance}"
+        s += f", visited: {self.__visited}"
+        s += f", color: '{self.__color}'"
         s += "})"
 
         return s
 
     def update_attributes(self, **kwargs: Unpack[VertexAttributes]):
         """Updates the properties of the vertex."""
+        if '__edges' in kwargs:
+            return
+
         for key, value in kwargs.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
+            attribute_key = f"_{self.__class__.__name__}__{key}"
+
+            if hasattr(self, attribute_key):
+                setattr(self, attribute_key, value)
 
     def add_edge(self, edge: 'Edge'):
         """Adds an edge to the vertex."""
-        self.edges.append(edge)
+        self.__edges.append(edge)
+
+    def get_label(self) -> str:
+        """Returns the label of the vertex."""
+        return self.__label
+
+    def get_edges(self) -> list['Edge']:
+        """Returns a list of edges connected to the vertex."""
+        return self.__edges
+
+    def get_color(self) -> str:
+        """Returns the color of the vertex."""
+        return self.__color
+
+    def get_distance(self) -> int | float:
+        """Returns the distance from the start vertex."""
+        return self.__distance
 
     def get_neighbors(self) -> list['Vertex']:
         """Returns a list of neighboring vertices."""
-        return [edge.destination for edge in self.edges]
+        return [edge.destination for edge in self.__edges]
 
 class Edge:
     """A class representing an edge in a graph."""
@@ -61,7 +87,7 @@ class Edge:
 
     def __repr__(self) -> str:
         """Returns a string representation of the edge."""
-        return f"Edge({self.source.label}, {self.destination.label}, {self.weight})"
+        return f"Edge({self.source.get_label()}, {self.destination.get_label()}, {self.weight})"
 
     def update_weight(self, new_weight: int):
         """Updates the weight of the edge."""
