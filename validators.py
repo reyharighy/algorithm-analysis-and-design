4829f483d60ec6c9fcf89718223e9bf8a4d9cde1
reyharighy <arighymoch@gmail.com>
2025-06-02 13:58:48 +0700

    helper function
    module to provide helper related function when performing many cases in graph

---
+++ validators.py
@@ -0,0 +1,21 @@
+"""A module to help validate labels of vertices and edges in a graph."""
+
+def validate_labels(*label_args):
+    """Decorator to validate labels of vertices and edges."""
+    def decorator(func):
+        def wrapper(self, *args, **kwargs):
+            arg_names = func.__code__.co_varnames[:func.__code__.co_argcount]
+            arg_map = dict(zip(arg_names, (self, *args))) | kwargs
+
+            if 'edges' in arg_map:
+                return None
+
+            for label in label_args:
+                value = arg_map.get(label)
+
+                if not isinstance(value, str) or not value.isalpha() or len(value) == 0:
+                    return None
+
+            return func(self, *args, **kwargs)
+        return wrapper
+    return decorator
