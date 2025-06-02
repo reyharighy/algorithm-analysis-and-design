5a369458dc9ac24708ad2d56489df1e8904ede51
reyharighy <arighymoch@gmail.com>
2025-06-02 17:13:57 +0700

    implement the bfs class
    it's still in the beginning process

--- main.py
+++ main.py
@@ -1,26 +1,15 @@
 """Main module to run the graph operations."""
+
 from breadth_first_search import BreadthFirstSearch
 
 bfs = BreadthFirstSearch()
+bfs.create_graph_from_problem_statement()
 
-for i in range(65, 75):
-    bfs.add_vertex(chr(i))
+for vertex in bfs.get_vertices():
+    print(vertex)
 
-edge_dictionary = {
-    'A': ['B', 'D', 'E'],
-    'B': ['C'],
-    'C': ['G', 'H'],
-    'D': ['F'],
-    'E': ['D', 'F'],
-    'F': ['J'],
-    'G': [],
-    'H': ['G'],
-    'I': ['G', 'H'],
-    'J': [],
-}
+bfs.run('A')
 
-for source, dest in edge_dictionary.items():
-    for d in dest:
-        bfs.add_edge(source, d)
-
-print(bfs)
+print('')
+for vertex in bfs.get_vertices():
+    print(vertex)
