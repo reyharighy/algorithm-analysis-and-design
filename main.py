06dd177c4061d1bd83b1c36cc36c4b0489534e38
reyharighy <arighymoch@gmail.com>
2025-06-03 17:17:50 +0700

    experiment with BFS and DFS implementation
    so far it works as expected

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 """Main module to run the graph operations."""
 
 from breadth_first_search import BreadthFirstSearch
+from depth_first_search import DepthFirstSearch
 
 bfs = BreadthFirstSearch()
 bfs.create_graph_from_problem_statement(1)
 
+print("Before BFS traversal:")
 for vertex in bfs.get_vertices():
-    print(vertex)
+    print(vertex, end=' ')
+    print(f"({{distance: {vertex.get_distance()}}})")
 
 bfs.run('A')
 
-print('')
+print("\nAfter BFS traversal:")
 for vertex in bfs.get_vertices():
-    print(vertex)
+    print(vertex, end=' ')
+    print(f"({{distance: {vertex.get_distance()}}})")
+
+dfs = DepthFirstSearch()
+dfs.create_graph_from_problem_statement(1)
+
+print("\nBefore DFS traversal:")
+for vertex in dfs.get_vertices():
+    print(vertex, end=' ')
+
+    S = "({{"
+    S += f"discovery_time: {vertex.get_discovery_time()}, "
+    S += f"finish_time: {vertex.get_finish_time()}"
+    S += "}})"
+
+    print(S)
+
+dfs.run('A')
+
+print("\nAfter DFS traversal:")
+for vertex in dfs.get_vertices():
+    print(vertex, end=' ')
+
+    S = "({{"
+    S += f"discovery_time: {vertex.get_discovery_time()}, "
+    S += f"finish_time: {vertex.get_finish_time()}"
+    S += "}})"
+
+    print(S)
