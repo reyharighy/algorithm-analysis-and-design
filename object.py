e75465e6cd1c4cb5225764c93f5a0d94e1700fb9
reyharighy <arighymoch@gmail.com>
2025-06-03 17:06:27 +0700

    DFS and BFS update
    - add getter functions to every attributes existing in BFS and DFS
    - add updater functions to attributes separately either in BFS and DFS
    - improve the interface for BFS and DFS dataclasses
    - make a clear separation between the default attributes and specific attributes in BFS and DFS

--- object.py
+++ object.py
@@ -1,32 +1,55 @@
 """This module defines objects used in graph representation, including Vertex and Edge classes."""
 
 from typing import TypedDict, Unpack
+from dataclasses import dataclass
 
-class VertexAttributes(TypedDict, total=False):
+class VertexDefaultAttributes(TypedDict, total=False):
     """A TypedDict for vertex attributes."""
     label: str
-    predecessor: 'Vertex | None'
-    distance: int | float
-    visited: bool
     color: str
+    predecessor: 'Vertex | None'
+
+class BFSUniqueAttributes(TypedDict, total=False):
+    """A TypedDict for BFS unique attributes."""
+    distance: int | float
+
+class DFSUniqueAttributes(TypedDict, total=False):
+    """A TypedDict for DFS unique attributes."""
+    discovery_time: int | float
+    finish_time: int | float
+
+@dataclass
+class BFSAttributes:
+    """A dataclass for BFS attributes."""
+    distance: int | float = float('inf')
+
+@dataclass
+class DFSAttributes:
+    """A dataclass for DFS attributes."""
+    discovery_time: int | float = 0
+    finish_time: int | float = 0
 
 class Vertex:
     """A class representing a vertex in a graph."""
 
     def __init__(self, label: str):
+        # Default attributes in Basic Graph
         self.__label: str = label
+        self.__color: str = 'white'
+        self.__predecessor: 'Vertex | None' = None
         self.__edges: list[Edge] = []
 
-        # Default attributes
-        self.__predecessor: Vertex | None = None
-        self.__distance: int | float = float('inf')
-        self.__visited: bool = False
-        self.__color: str = 'white'
+        # BFS-specific attributes
+        self.__bfs_attributes: BFSAttributes = BFSAttributes()
+
+        # DFS-specific attributes
+        self.__dfs_attributes: DFSAttributes = DFSAttributes()
 
     def __repr__(self) -> str:
         """Returns a string representation of the vertex."""
         s = "Vertex({"
         s += f"label: {self.__label}"
+        s += f", color: '{self.__color}'"
         s += ", predecessor: "
 
         if self.__predecessor:
@@ -35,23 +58,9 @@ class Vertex:
             s += "None"
 
         s += f", edges: {self.__edges}"
-        s += f", distance: {self.__distance}"
-        s += f", visited: {self.__visited}"
-        s += f", color: '{self.__color}'"
         s += "})"
 
         return s
-
-    def update_attributes(self, **kwargs: Unpack[VertexAttributes]):
-        """Updates the properties of the vertex."""
-        if '__edges' in kwargs:
-            raise ValueError("Cannot update edges directly.")
-
-        for key, value in kwargs.items():
-            attribute_key = f"_{self.__class__.__name__}__{key}"
-
-            if hasattr(self, attribute_key):
-                setattr(self, attribute_key, value)
 
     def add_edge(self, edge: 'Edge'):
         """Adds an edge to the vertex."""
@@ -65,17 +74,58 @@ class Vertex:
         """Returns a list of edges connected to the vertex."""
         return self.__edges
 
+    def get_predecessor(self) -> 'Vertex | None':
+        """Returns the predecessor of the vertex."""
+        return self.__predecessor
+
     def get_color(self) -> str:
         """Returns the color of the vertex."""
         return self.__color
 
-    def get_distance(self) -> int | float:
-        """Returns the distance from the start vertex."""
-        return self.__distance
-
     def get_neighbors(self) -> list['Vertex']:
-        """Returns a list of neighboring vertices."""
+        """Returns a list of neighboring vertices. Used specifically when traversing the graph."""
         return [edge.destination for edge in self.__edges]
+
+    def get_distance(self) -> int | float:
+        """Returns the BFS distance of the vertex."""
+        return self.__bfs_attributes.distance
+
+    def get_discovery_time(self) -> int | float:
+        """Returns the DFS discovery time of the vertex."""
+        return self.__dfs_attributes.discovery_time
+
+    def get_finish_time(self) -> int | float:
+        """Returns the DFS finish time of the vertex."""
+        return self.__dfs_attributes.finish_time
+
+    def update_default_attributes(self, **kwargs: Unpack[VertexDefaultAttributes]):
+        """Updates the properties of the vertex."""
+        if '__edges' in kwargs:
+            raise ValueError("Cannot update edges directly.")
+
+        for key, value in kwargs.items():
+            attribute_key = f"_{self.__class__.__name__}__{key}"
+
+            if hasattr(self, attribute_key):
+                setattr(self, attribute_key, value)
+            else:
+                raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{key}'")
+
+    def update_bfs_attributes(self, **kwargs: Unpack[BFSUniqueAttributes]):
+        """Updates BFS-specific attributes of the vertex."""
+        for key, value in kwargs.items():
+            if hasattr(self.__bfs_attributes, key):
+                setattr(self.__bfs_attributes, key, value)
+            else:
+                raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{key}'")
+
+    def update_dfs_attributes(self, **kwargs: Unpack[DFSUniqueAttributes]):
+        """Updates DFS-specific attributes of the vertex."""
+        for key, value in kwargs.items():
+            if hasattr(self.__dfs_attributes, key):
+                setattr(self.__dfs_attributes, key, value)
+            else:
+                raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{key}'")
 
 class Edge:
     """A class representing an edge in a graph."""
